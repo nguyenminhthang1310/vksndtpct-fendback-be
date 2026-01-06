1e93d7085560c50d22f34201059e0d025c6572b5
nguyenminhthang1310 <minhthangnguyen1310@gmail.com>
2026-01-06 16:24:49 +0700

    .

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ Kết nối MongoDB thành công!");
+    console.log("✅ Kết nối MongoDB thành công rồi!");
   } catch (error) {
     console.error("❌ Lỗi kết nối MongoDB:", error.message);
     process.exit(1);
